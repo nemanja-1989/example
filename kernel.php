29e21f2f13155c0f8b68015c11243c6c36bbdbfb
Nemanja Jocic <nemanja_jocic@yahoo.com>
2022-11-23 13:02:18 +0100

    chg

--- kernel.php
+++ kernel.php
@@ -2,10 +2,7 @@
 
 require_once dirname(__DIR__) . '/loopia/vendor/autoload.php';
 
-use Loopia\App\Api\Client;
-use Loopia\App\Api\FilmApiDataCache;
 use Loopia\App\Console\Schedule;
-use Loopia\App\Api\FilmApiDataLoader;
 
 $schedule = new Schedule();
 $schedule->exe();
