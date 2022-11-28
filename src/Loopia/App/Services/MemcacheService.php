19ea1c469027754af31eabfc8424bc136f68c70a
Nemanja Jocic <nemanja_jocic@yahoo.com>
2022-11-28 20:56:14 +0100

    memcache

--- src/Loopia/App/Services/MemcacheService.php
+++ src/Loopia/App/Services/MemcacheService.php
@@ -7,6 +7,8 @@ use Loopia\App\Interface\ServiceInterface;
 class MemcacheService implements ServiceInterface{
     
     public function getService() {
-        return new \Memcache();
+        $memcache = new \Memcache();
+        $memcache->addServer('127.0.0.1', 11211);
+        return $memcache;
     }
 }
